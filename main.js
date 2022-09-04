fcd39a2e437a424d50aa16bfd29cd4bf3fb4eaf7
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-04 18:12:50 +0300

    Added the Sound effects

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 window.addEventListener('keydown', playSound);
 
 function playSound(event) {
-    console.log(event.keyCode)
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    key.classList.add('audioEffect')
+    if (audio === null) return;
+    audio.currentTime = 0;
+    audio.play();
 }
+
+function removeBorder(event) {
+    if (event.propertyName !== "transform") return;
+    this.classList.remove('audioEffect');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeBorder))
